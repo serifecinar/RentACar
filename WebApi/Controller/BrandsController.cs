bd2b728a8e23593488c28254c7441a61e6a636b1
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-09-14 02:46:21 +0300

    Add project files.

---
+++ WebApi/Controller/BrandsController.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Brands.Command.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+            return Ok(response);
+        }
+    }
+}
